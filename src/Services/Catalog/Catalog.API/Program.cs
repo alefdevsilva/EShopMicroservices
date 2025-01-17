6347637d913c7438c4119608b14aa4a302632916
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-01-16 22:37:19 -0300

    - Remove Manual Validator and Test Validation Pipeline Behavior with MediatR

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,18 +1,17 @@
 using BuildingBlocks.Behaviors;
-using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
-
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
