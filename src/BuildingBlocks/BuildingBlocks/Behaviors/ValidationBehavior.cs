549632a8b7770c504df558c2a6d6f8f6ce0a28b9
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-01-16 22:29:34 -0300

    - Validation Pipeline Behavior with MediatR using Fluent Validation Library

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+using System.Linq;
+using System.Windows.Input;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<TResquest, TResponse>
+    (IEnumerable<IValidator<TResquest>> validatiors)
+    : IPipelineBehavior<TResquest, TResponse>
+    where TResquest : ICommand<TResponse>
+{
+    public async Task<TResponse> Handle(TResquest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TResquest>(request);
+        var validationResults = await Task.WhenAll(validatiors.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults
+            .Where(r => r.Errors.Any())
+            .SelectMany(r => r.Errors)
+            .ToList();
+
+        if (failures.Any())
+            throw new ValidationException(failures);
+
+        return await next();
+    }
+}
