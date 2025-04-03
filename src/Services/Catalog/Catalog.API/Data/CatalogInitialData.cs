85203cf7680e22b41eb23d843cb34a4e07aacc57
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-04-03 11:44:46 -0300

    - Seeding CatalogDb with Marten Initial Baseline Data (IInitialData interface)

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,40 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data;
+
+public class CatalogInitialData : IInitialData
+{
+    public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+    {
+        using var session = store.LightweightSession();
+
+        if (await session.Query<Product>().AnyAsync())
+            return;
+
+        // Marten UPSERT will cater for existing records
+        session.Store<Product>(GetPreconfiguredProducts());
+        await session.SaveChangesAsync();
+    }
+
+    private static IEnumerable<Product> GetPreconfiguredProducts() => new List<Product>
+    {
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "IPhone X",
+            Description = "Description",
+            ImageFile = "product-1.png",
+            Price = 950.00M,
+            Category = new List<string>{ "Smart Phone" }
+        },
+         new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Samsung S24",
+            Description = "Description",
+            ImageFile = "product-1.png",
+            Price = 850.00M,
+            Category = new List<string>{ "Smart Phone" }
+        }
+    };
+}
