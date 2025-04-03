ce0797bb11a03f640f89fb419405d8c04cabbe19
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-04-03 11:15:37 -0300

    - Removing dependency injection from commandhandler classes

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -1,7 +1,4 @@
-﻿
-using Catalog.API.Products.UpdateProduct;
-
-namespace Catalog.API.Products.DeleteProduct;
+﻿namespace Catalog.API.Products.DeleteProduct;
 
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
@@ -15,13 +12,11 @@ public class DeleteProductCommandValidator : AbstractValidator<DeleteProductComm
 }
 
 internal class DeleteProductCommandHandler
-    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    (IDocumentSession session)
     : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
     public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
     {
-        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
-
         session.Delete<Product>(command.Id);
         await session.SaveChangesAsync(cancellationToken);
 
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -6,15 +6,12 @@ public record GetProductsQuery() : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> products);
 
 internal class GetProductsQueryHandler
-    (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    (IDocumentSession session)
     : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductsQueryHandler.Handler called with {@Query}", query);
-
         var products = await session.Query<Product>().ToListAsync(cancellationToken);
-
         return new GetProductsResult(products);
     }
 }
