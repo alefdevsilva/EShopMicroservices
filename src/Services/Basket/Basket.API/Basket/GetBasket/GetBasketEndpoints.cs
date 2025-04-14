ccb599867432c26ab50dc0d42ec6d61f6584e0a3
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-04-14 00:16:13 -0300

    - Develop Get Basket Endpoint with Minimal Apis and Carter

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -1,5 +1,29 @@
-﻿namespace Basket.API.Basket.GetBasket;
+﻿using Basket.API.Models;
+using Carter;
+using Mapster;
+using MediatR;
 
-public class GetBasketEndpoints
+namespace Basket.API.Basket.GetBasket;
+
+//public record GetBasketRequest(string UserName);
+
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndpoints : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName));
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
 }
