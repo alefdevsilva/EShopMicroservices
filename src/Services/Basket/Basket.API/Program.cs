e48ce9ec9b4d002a8d27864ab991c6e741ade272
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-04-28 20:01:09 -0300

    - Register MediatR and Cardter libraries into Asp.Net Dependency Injection Service

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,21 @@
+using BuildingBlocks.Behaviors;
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
