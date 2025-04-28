7a72da15d4aed41fe2bdb049185196a9e8de8fc2
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2025-04-27 22:26:06 -0300

    - Develop StoreBasket Feature Handler class with CQRS and MediatR

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,31 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+using FluentValidation;
 
-public class StoreBasketHandler
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.UserName).NotNull().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler 
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        //TODO: store basket in database(use Marten upsert - if exist = update, if not exist insert)
+        //TODO: update cache
+
+        return new StoreBasketResult("UserName");
+     }
 }
