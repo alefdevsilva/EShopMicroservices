09261e4cdcc41dd3d897745836773bf84a35f86c
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2026-01-07 23:17:49 -0300

    Develop DDD Abstration (Entity, Value Objecs, Aggregates, Agg.Roots)

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime? CreateAt { get; set; }
+    public string? CreateBt { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
