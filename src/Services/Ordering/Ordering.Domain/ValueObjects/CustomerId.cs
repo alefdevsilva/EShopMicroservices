e67a8d6bd491bd3919abb50bd5351816ff6c3577
Alef Silva <56990114+alefdevsilva@users.noreply.github.com>
2026-01-11 21:25:08 -0300

    - Develop DDD Rich-domain model Entity for Order Entities in Order.Domain Layer

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -1,6 +1,20 @@
-﻿namespace Ordering.Domain.ValueObjects;
+﻿using Ordering.Domain.Exceptions;
+
+namespace Ordering.Domain.ValueObjects;
 
 public record CustomerId
 {
     public Guid Value { get; }
+    private CustomerId(Guid value) => Value = Value;
+
+    public static CustomerId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value); 
+        if(value == Guid.Empty)
+        {
+            throw new DomainException("CustomerId cannot br empty.");
+        }
+
+        return new CustomerId(value);
+    }
 }
